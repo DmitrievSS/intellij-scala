d39af9af2d85e39708a2d431bb7d7c8e232bd313
ssdmitriev <ssdmitriev@yandex-team.ru>
2016-02-11 05:17:33 +0300

    fix of perfomance

--- src/org/jetbrains/plugins/scala/codeInspection/dataFlow/UnusedFunctionParameterInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/dataFlow/UnusedFunctionParameterInspection.scala
@@ -9,6 +9,9 @@ import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter
 import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunctionDefinition, ScFunction}
 import org.jetbrains.plugins.scala.lang.psi.impl.statements.params.ScParameterImpl
 
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
 
 /**
   * Created by ssdmitriev on 04.02.16.
@@ -17,27 +20,24 @@ class UnusedFunctionParameterInspection
   extends AbstractInspection("UnusedFunctionParameter", "function parameter not used") {
   override def actionFor(holder: ProblemsHolder): PartialFunction[PsiElement, Any] = {
     case expr: ScFunctionDefinition =>
-      expr.parameters.foreach(parameter =>
-        if (findUnusedParameter(expr, parameter)) {
-          val message = "function parameter not used"
-          holder.registerProblem(parameter, message)
-        })
+      val message = "function parameter not used"
+      findUnusedParameter(expr).foreach(parameter => holder.registerProblem(parameter, message))
   }
 
-  def findUnusedParameter(f: ScFunctionDefinition, parameter: ScParameter): Boolean = {
-    var unusedParameter = true
+  def findUnusedParameter(f: ScFunctionDefinition): mutable.Buffer[ScParameter] = {
+    val unusedParameters = f.parameters.toBuffer
     val visitor = new ScalaRecursiveElementVisitor() {
       override def visitReferenceExpression(ref: ScReferenceExpression): Unit = {
-        if (ref.resolve().getText == parameter.getText) {
+        if (unusedParameters.map(el=>el.getText.substring(0, el.getText.indexOf(':'))).contains(ref.getText)) {
           ref.resolve() match {
             case p: ScParameterImpl =>
-              unusedParameter = false
+              unusedParameters-=(p)
             case _ =>
           }
         }
       }
     }
     f.body.foreach(_.accept(visitor))
-    unusedParameter
+    unusedParameters
   }
 }
